fd20dfdfead309527269b067f602043cf91abe96
Yesheng Zhong <zhongyeshengd@gmail.com>
2025-12-09 19:43:12 +1030

    add routes, models, and controllers for users to login and logout

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,5 +2,14 @@
 
 use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
-Route::apiResource('jobs', JobController::class);
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login',    [AuthController::class, 'login']);
+
+// TODO: what will happen when no middleware for jobcontroller
+Route::middleware('auth:api')->group(function () {
+    // Route::get('/me',  [AuthController::class, 'me']);
+    Route::apiResource('jobs', JobController::class);
+});
